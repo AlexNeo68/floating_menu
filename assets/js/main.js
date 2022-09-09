27a8dd726d468d676b6c65e39ae0294d760aabf4
Александр Неудахин <aleksandrneudahin@Aleksandrs-Air.localnet>
2022-09-09 08:49:16 +0300

    second

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/*=============== SHOW & HIDE MENU ===============*/
+
+const toggleButton = document.getElementById("floating-toggle");
+
+const activeMenu = () => {
+  toggleButton.classList.toggle("active");
+};
+
+toggleButton.addEventListener("click", activeMenu);
